3e6fe961360321db75aa6935bb3576c4b47db17e
touhouota <touhouota@touhouota.local>
2017-11-19 17:13:51 +0900

    無駄にforEachをしていたのを修正

--- js/todo_page.js
+++ js/todo_page.js
@@ -12,17 +12,19 @@ window.onload = function() {
 			console.table(response.data);
 			if (response.ok) {
 				let data = response.data;
-				data.forEach(function(item) {
-					// todoの作成
-					let todo = data.filter(Task.filter("todos"));
-					let todo_list = Task.create_task_list(todo);
-					document.getElementById("todos").appendChild(todo_list);
 
-					// doneの作成
-					let done = data.filter(Task.filter("dones"));
-					let done_list = Task.create_task_list(done);
-					document.getElementById("dones").appendChild(done_list);
-				});
+				// todoの作成
+				let todo = data.filter(Task.filter("todos"));
+				console.log(todo);
+				let todo_list = Task.create_task_list(todo);
+				document.getElementById("todos").appendChild(todo_list);
+
+				// doneの作成
+				let done = data.filter(Task.filter("dones"));
+				console.log(done);
+				let done_list = Task.create_task_list(done);
+				document.getElementById("dones").appendChild(done_list);
+
 			}
 		}
 	}).send(null);
