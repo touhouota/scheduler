a739ad9558ade47757cef1e71a6e460c0d81faac
touhouota <touhouota.register@gmail.com>
2017-12-06 10:07:43 +0900

    サブタスク数を数えるオブザーバ、タスク数を数えるオブザーバを設置

--- js/todo_page_simple.js
+++ js/todo_page_simple.js
@@ -12,4 +12,28 @@ window.onload = function() {
 	document.getElementById("github").addEventListener("click", Base.request);
 	// 仲間
 	document.getElementById("member").addEventListener("click", Base.member);
+
+	// タスクを監視し、変化があれば数を数え直す
+	new MutationObserver(Task.progress_count).observe(document.querySelector(".todo_area"), {
+		childList: true,
+		subtree: true,
+	});
+	// タスクを監視し、変化があればサブタスク数を数える
+	new MutationObserver(Task.subtask_count).observe(document.querySelector(".todo_area"), {
+		childList: true,
+		subtree: true,
+	});
+
+
+	// コメント入力欄のイベント
+	// 入力欄の状態によってボタンの表示を変える
+	document.getElementById("comment").addEventListener("input", Timeline.check_comment_textarea);
+	// コメントの送信を行うためのイベント設定
+	const button = document.getElementById("comment_button");
+	button.addEventListener("click", Timeline.append);
+	button.disabled = true;
+
+
+	// このページでNotificationAPIを使うための準備
+	Notify.init();
 };
