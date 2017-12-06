727bb0e8c94b198d81cd3a9c2de7d2915af482e1
touhouota <touhouota.register@gmail.com>
2017-12-06 09:44:56 +0900

    親、子をそれぞれ取得するためのJS_APIもどきを作成

--- js/todo_simple.js
+++ js/todo_simple.js
@@ -1,6 +1,7 @@
 let Task = {
 	parent: [],
 	child: [],
+	tree: {},
 	create_task_list: function(task_list) {
 		let fragment = document.createDocumentFragment();
 		if (!task_list) {
@@ -33,6 +34,75 @@ let Task = {
 		}
 		// タスクの作成
 		return task;
+	},
+
+	get_parents: function() {
+		const query = "?cmd=task_parent&user_id=" + Base.get_cookie("user_id");
+		Base.create_request("GET", Base.request_path + query, function() {
+			if (this.status == 200 && this.readyState == 4) {
+				let response = JSON.parse(this.responseText);
+				// console.table(response.data);
+				if (response.ok) {
+					let data = response.data;
+					let fragment = document.createDocumentFragment();
+					for (let i = 0; i < data.length; i++) {
+						let task_info = data[i];
+						let task = Task.create_task(task_info);
+						Task.tree[task_info.task_id] = [];
+						Task.parent.push(task_info.task_id);
+
+						fragment.appendChild(task);
+					}
+					document.getElementById("todos").appendChild(fragment);
+					// 親の処理が終われば、子供を取得
+					Task.get_child();
+				}
+			}
+		}).send(null);
+	},
+
+	get_child: function() {
+		let parents = Object.keys(Task.tree);
+		console.log(parents);
+		parents.forEach(function(parent_id) {
+			let query = [
+				"?cmd=task_child",
+				"&parent=", parent_id,
+				"&user_id=" + Base.get_cookie("user_id"),
+			].join("");
+			Base.create_request("GET", Base.request_path + query, function() {
+				if (this.status == 200 && this.readyState == 4) {
+					let response = JSON.parse(this.responseText);
+					if (response.ok) {
+						let data = response.data;
+						console.table(data);
+
+						// 子供がいない場合は、何もしない
+						if (response.data.length === 0) {
+							return;
+						}
+						// 子供をひとまとめにするfragment作成
+						let fragment = document.createDocumentFragment();
+
+						data.forEach(function(item) {
+							Task.tree[item.parent].push(item.task_id)
+							Task.child.push(item.task_id);
+							let subtask = Task.create_task(item);
+							subtask.classList.add("sub");
+							fragment.appendChild(subtask);
+						});
+
+						let sub = document.getElementById("task_id:" + data[0].parent).querySelector(".subtask_list");
+						sub.appendChild(fragment);
+
+						let tasks = document.querySelectorAll(".task");
+						for (i = 0; i < tasks.length; i++) {
+							ProgressTimer.display(tasks[i]);
+						}
+					}
+				}
+			}).send(null);
+		});
 	},
 
 	// 引数の状態により、filterを返す
