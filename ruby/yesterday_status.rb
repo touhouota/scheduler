5fd068d7c0354436e89498200e4b95e90abc9649
touhouota <touhouota.register@gmail.com>
2017-12-10 00:31:29 +0900

    planner_XXXを次世代のメール送り装置とするため、ファイルのリネームを行った。 なお、古いものはold送り。

--- ruby/yesterday_status.rb
+++ ruby/yesterday_status.rb
@@ -1,36 +1,47 @@
-
-require 'date'
-require './mail'
+require 'time'
 require 'mysql2'
+require 'date'
+require 'yaml'
+require './mail'
+require './mail_content_creater'
 
-mail = SendMail.new(to: 'g2117034@fun.ac.jp', from: 'ika@sketch.jp', option: { server: 'po.sketch.jp', port: 587, ssl: false })
+######
+# main
+######
 
-# subject
-yesterday = DateTime.now - 1
-subject = "#{yesterday.strftime('%m月%d日')}のみんなの様子 - スケジューラ"
+client = Mysql2::Client.new(YAML.load_file('./database_info.yaml'))
+# デフォルトでkeyをsymbolにする
+client.query_options[:symbolize_keys] = true
 
-# body
-client = Mysql2::Client.new(host: 'localhost', username: 'research', password: 'do_research', encoding: 'utf8', database: 'do_research')
-yesterday_str = yesterday.strftime('%Y-%m-%d')
-sql = "select * from task join users using(user_id) where modify = Date('#{yesterday_str}')"
-yesterday_tasks = client.query(sql).entries
-users = yesterday_tasks.each_with_object({}) do |entry, hash|
-  hash[entry['name']] = { tasks: 0, fin: 0 } unless hash.dig(entry['name'])
-  # ユーザ名を数える => その日のタスク数がわかる
-  hash[entry['name']][:tasks] += 1
-  # statusが2のものを数える => 完了した数がわかる
-  hash[entry['name']][:fin] += 1 if entry['status'] == 2
+# 今日、昨日の日付をyyyy-mm-ddで取得
+today = Date.today.strftime('%F')
+yesterday = (Date.today - 1).strftime('%F')
+
+client.query('select * from groups').each do |group|
+  # mailアドレスが設定されていない場合は無視する
+  next if group[:mail].nil?
+  # メールアドレスが設定されているグループには、そこあてにメールを送る
+  mail = SendMail.new(to: group[:mail], from: 'planner@sketch.jp', option: { server: 'po.sketch.jp', port: 587, ssl: false })
+
+  body = count_down(group[:group_name])
+  # グループに属している人のTL内容を取得
+  search_query = <<-SQL
+  select * from task_timeline join (task join users using(user_id)) using(task_id)
+  where users.group_id = ? and Date(task_timeline.created) between ? and ?
+  SQL
+
+  body += "昨日の#{group[:group_name]}の様子は以下の通りでした。\n\n"
+  result = client.prepare(search_query).execute(group[:group_id], yesterday, today)
+  body += if result.count == 0
+    "誰も活動していないようです...（´・ω・`）\n"
+          else
+    members_action(result)
+          end
+
+  body += "\n今日も頑張って行きましょう！\n"
+  body += 'http://mimalab.c.fun.ac.jp/b1013179/scheduler/'
+
+  title = group[:group_name] + 'の昨日のようす'
+
+  mail.send(subject: title, body: body)
 end
-body = "#{yesterday.strftime('%m月%d日')}の美馬研の様子は以下の通りでした。\n\n"
-if !users.empty?
-users.each do |user, info|
-  body += "・#{user}の活動\n"
-  body += "=>#{info[:tasks]}個のタスクを登録し、#{info[:fin]}個を終わらせました！\n\n"
-end
-else
-  body += "誰も活動していないようです...（´・ω・`）\n\n"
-end
-body += "今日も頑張って行きましょう！\n"
-body += 'http://mimalab.c.fun.ac.jp/b1013179/scheduler/list.html'
-
-mail.send(subject: subject, body: body)
