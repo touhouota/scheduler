2c09dd8a3af3f4091b21fd6d1b0be2e83add9b7e
touhouota <touhouota.register@gmail.com>
2017-12-10 12:13:35 +0900

    予定を読み込み、カウントダウンする処理をcsvからyamlに置き換えた。

--- ruby/mail_content_creater.rb
+++ ruby/mail_content_creater.rb
@@ -13,28 +13,19 @@ def count_down(group_name)
   today = Date.today
 
   # ファイルが存在しない時は、無視
-  return '' unless File.exist?("./plan/#{group_name}.csv")
-
+  file_path = "./plan/#{group_name}.yaml"
+  return '' unless File.exist?(file_path)
+  plan_list = YAML.load_file(file_path)
   # ファイルが有るならば、読み込む
-  CSV.foreach("./plan/#{group_name}.csv", csv_options) do |row|
-    date = Date.parse(row[:date])
-    case (date - today).to_i
+  plan_list.each do |row|
+    case (row[:date] - today).to_i
     when -Float::INFINITY..-1
       next
     when 0
       body << "・「#{row[:plan]}」当日。お疲れ様！！\n"
       body << "\n"
-    when 1..7
-      body << "・「#{row[:plan]}」まで、あと#{(date - today).to_i}日。\n"
-      body << "\n"
-    when 8..14
-      body << "・「#{row[:plan]}」まで、あと#{(date - today).to_i}日。\n"
-      body << "\n"
-    when 15..30
-      body << "・「#{row[:plan]}」まで、あと#{(date - today).to_i}日。\n"
-      body << "\n"
     else
-      body << "・「#{row[:plan]}」まで、あと#{(date - today).to_i}日。\n"
+      body << "・「#{row[:plan]}」まで、あと#{(row[:date] - today).to_i}日。\n"
       body << "\n"
     end
   end
@@ -56,7 +47,6 @@ def members_action(timeline)
   # TLから各ユーザのタスク追加・実行状況を計測
   users = timeline.each_with_object({}) do |entry, hash|
   hash[entry[:name]] = { tasks: 0, fin: 0 } unless hash.dig(entry[:name])
-  p entry
   # ユーザ名を数える => その日のタスク数がわかる
   hash[entry[:name]][:tasks] += 1 if entry[:status] == 0
   # statusが2のものを数える => 完了した数がわかる
