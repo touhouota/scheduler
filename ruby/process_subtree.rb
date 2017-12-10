4570c45ed4c3b52a5c5c1af80498d53f69a4a527
touhouota <touhouota.register@gmail.com>
2017-12-10 23:18:00 +0900

    親タスクの取得を、終わっていないものだけにした。

--- ruby/process_subtree.rb
+++ ruby/process_subtree.rb
@@ -130,8 +130,9 @@ def get_task_child(cgi)
   keys = %i[user_id parent]
   raise $error_string + ' (get_task_child)' unless _check_data(keys, cgi)
 
+  # 終わっていない親タスクを取得
   sql = <<-SQL
-  select * from task left outer join task_tree on task_id = child where (user_id = ? and parent = ?)
+  select * from task left outer join task_tree on task_id = child where (status in (0, 1, 4) and user_id = ? and parent is null)
   SQL
 
   result = $client.prepare(sql).execute(cgi[:user_id], cgi[:parent])
