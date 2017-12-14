c2e95078dda59fbfead92e45e0b1e3c9097f2ada
touhouota <touhouota.register@gmail.com>
2017-12-14 18:23:01 +0900

    タスク修正時、同じ処理を何度も繰り返して書いていたため、書き直した。

--- js/todo_modal.js
+++ js/todo_modal.js
@@ -232,18 +232,14 @@ let Modal = {
 				if (response.ok) {
 					const data = response.data;
 					let new_task = Task.create_task(data[0]);
+					let old_task = document.getElementById("task_id:" + data[0].task_id);
+					old_task.parentElement.replaceChild(new_task, old_task);
+
 					if (data[0].parent === null) {
-						// parentに値がなければ、それは親タスク
-						let old_task = document.getElementById("task_id:" + data[0].task_id);
-						old_task.parentElement.replaceChild(new_task, old_task);
+						// parentに値がなければ、親タスク
 						Task.get_child(data[0].task_id);
 					} else {
-						console.log(new_task);
-						// parentに値があれば、それはサブタスク
 						new_task.classList.add("sub");
-						// 自分自身を見つけて、入れ替える
-						let old_task = document.getElementById("task_id:" + data[0].task_id);
-						old_task.parentElement.replaceChild(new_task, old_task);
 					}
 				}
 				Modal.remove();
