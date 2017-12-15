fddcda1fe2c062d95852a800187b028be1d0bf7a
touhouota <touhouota.register@gmail.com>
2017-12-16 00:48:58 +0900

    テストでコメントをぶち込むスクリプトを修正。

--- ruby/comment_inserter.rb
+++ ruby/comment_inserter.rb
@@ -1,12 +1,13 @@
 require 'mysql2'
 require 'yaml'
-require 'process_subtree'
+require './process_subtree'
 
 $client = Mysql2::Client.new(YAML.load_file('./database_info.yaml'))
 # デフォルトでkeyをsymbolにする
 $client.query_options[:symbolize_keys] = true
 
-num = 10
+print '繰り返す回数は？:'
+num = gets.to_i
 
 num.times do |i|
   p insert_timeline(user_id: 1_013_179, comment: "#{i}: #{Time.now.strftime('%F %T')}")
