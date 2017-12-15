ec93668d3bfc1f4fff6464ba7e644860843c050d
touhouota <touhouota.register@gmail.com>
2017-12-15 22:47:57 +0900

    通知が許可されていなければalertで通知出すというクソ仕様を修正。

--- js/timeline_list_test.js
+++ js/timeline_list_test.js
@@ -11,7 +11,7 @@ let Timeline = {
 		// item_list.forEach(function(comment_item) {
 		let i = 0;
 		let length = item_list.length;
-		for (i = 0; i < length; i++) {
+		for (i = 0; i < length; i += 1) {
 			let comment = template.cloneNode(true);
 			let comment_item = item_list[i];
 			// tl_id:xxxxとなる
@@ -30,10 +30,7 @@ let Timeline = {
 
 			// コメントの内容を記載
 			comment.querySelector(".comment_text").innerHTML = comment_item.items;
-			// console.log("create_item: ", comment_item.items);
-			if (Notification.permission === 'granted') {
-				Notify.create_instance(comment_item.items);
-			}
+			Notify.create_instance(comment_item.items);
 			fragment.insertBefore(comment, fragment.firstElementChild);
 		};
 
